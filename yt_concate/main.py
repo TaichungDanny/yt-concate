78fce2fbc6a7019e352289534366380687260561
TaichungDanny <52984695+TaichungDanny@users.noreply.github.com>
2023-02-18 11:41:39 +0800

    Update main.py

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -8,7 +8,7 @@ CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 
 def get_all_video_in_channel(channel_id):
-    api_key = 'AIzaSyATq3POltH5jmHUoDaj7eee3ZSX7_bhl8Q'
+    api_key = 
 
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
